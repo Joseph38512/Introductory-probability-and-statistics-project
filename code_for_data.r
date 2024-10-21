3e2eed5474f9cfc89782907d496a8cf6577fa521
Joseph38512 <s1910643@ed.ac.uk>
2024-10-21 15:32:05 +0100

    Calculated IDs for females and males

--- code_for_data.r
+++ code_for_data.r
@@ -13,6 +13,15 @@ data1[, c("PatientID","Biomarker") := tstrsplit(Biomarker, "-",
 
 
 
-print(data1[PatientID==5])
+males <- data2['Sex (1=male, 2=female)']==1
+females <- data2['Sex (1=male, 2=female)']==2
+
+males <- data2[males,]
+females <- data2[females,]
+males_ID <- males['PatientID']
+females_ID <- females['PatientID']
+print(males_ID)
+
+
 
 
