1b6a3b68c0361f2212fbea894348d5a0463fe7c5
Joseph38512 <s1910643@ed.ac.uk>
2024-11-13 20:05:59 +0000

    Added linear regression

--- code_for_data.r
+++ code_for_data.r
@@ -35,17 +35,31 @@ covariates_merged <- covariates_merged[covariates_merged["Biomarker"] == "0weeks
 
 
 
-
-print(covariates_merged["Smoker (1=yes, 2=no)"])
-
-#Create a regression model.
-
-# model = lm(`Vas-12months` ~ `Biomarker` + `Sex (1=male, 2=female)`+`Age`+`Smoker (1=yes, 2=no)`,covariates_merged)
-# print(model)
+length <- nrow(covariates_merged)
 
 
+# Calculate the index for first 80%
+eighty_percent <- floor(0.8 * length)
+
+# Split the data frame into first 80% and last 20%.
+eighty_percent_covariates <- covariates_merged[1:eighty_percent, ]
+twenty_percent_covariates <- covariates_merged[(eighty_percent + 1):length, ]
 
 
+#Create a linear regression model from 80% of the data.
+model <- lm(`Vas-12months` ~ `Sex (1=male, 2=female)`+ Age +`Smoker (1=yes, 2=no)`,eighty_percent_covariates)
+
+#Use the model to predict the next values.
+predictions <- predict(model, newdata = twenty_percent_covariates)
+
+#Print the differences in value.
+print(c(abs(predictions - twenty_percent_covariates["Vas-12months"])))
+
+#Print the average difference.
+print(mean(c(abs(predictions - twenty_percent_covariates["Vas-12months"]))))
+
+#Print first 7 to be put into a table to show prediction.
+print(twenty_percent_covariates[1:7,c(1, 6)])
 
 
 
@@ -62,7 +76,7 @@ male_data_inclusion <- male_data[Biomarker == '0weeks',]
 female_data_inclusion <- female_data[Biomarker == '0weeks',]
 
 
-
+#Perform a t test for each biomarker to test average of males vs females.
 IL_8 <- t.test(male_data_inclusion[['IL-8']],female_data_inclusion[['IL-8']])
 VegaF_A <- t.test(male_data_inclusion[['VEGF-A']],female_data_inclusion[['VEGF-A']])
 OPG <- t.test(male_data_inclusion[['OPG']],female_data_inclusion[['OPG']])
@@ -72,23 +86,51 @@ CXCL9 <- t.test(male_data[['CXCL9']],female_data[['CXCL9']])
 CXCL1 <- t.test(male_data[['CXCL1']],female_data[['CXCL1']])
 IL_18 <- t.test(male_data[['IL-18']],female_data[['IL-18']])
 CSF_1 <- t.test(male_data[['CSF-1']],female_data[['CSF-1']])
-CXCL1 <- t.test(male_data[['CXCL1']],female_data[['CXCL1']],p.adjust.method="bonferroni")
 
 
-par(mfrow = c(3, 6))
-dev.new(width=300, height=150, unit="px")
-hist(male_data[['IL-8']])
-hist(female_data[['IL-8']])
-hist(male_data[['VEGF-A']])
-hist(female_data[['VEGF-A']])
-hist(male_data[['OPG']])
-hist(female_data[['OPG']])
-hist(male_data[['TGF-beta-1']])
-hist(female_data[['TGF-beta-1']])
-hist(male_data[['IL-6']])
-hist(female_data[['IL-6']])
-hist(male_data[['OPG']])
-hist(female_data[['OPG']])
+#Output the distrubutions.
+dev.new(width=5, height=10)
+png(filename = "plot_of_distrubutions.png",width = 600, height = 800)
+par(mfrow = c(5, 4))
+
+hist(male_data[['IL-8']],main = "Male data IL-8",xlab = "Levels of biomarker")
+
+hist(female_data[['IL-8']],main = "Female data IL-8",xlab = "Levels of biomarker")
+
+hist(male_data[['VEGF-A']],main = "Male data VEGF-A",xlab = "Levels of biomarker")
+
+hist(female_data[['VEGF-A']],main = "Female data VEGF-A",xlab = "Levels of biomarker")
+
+hist(male_data[['OPG']],main = "Male data OPG",xlab = "Levels of biomarker")
+
+hist(female_data[['OPG']],main = "Female data OPG",xlab = "Levels of biomarker")
+
+hist(male_data[['TGF-beta-1']],main = "Male data TGF-beta-1",xlab = "Levels of biomarker")
+
+hist(female_data[['TGF-beta-1']],main = "Female data TGF-beta-1",xlab = "Levels of biomarker")
+
+hist(male_data[['IL-6']],main = "Male data IL-6",xlab = "Levels of biomarker")
+
+hist(female_data[['IL-6']],main = "Female data IL-6",xlab = "Levels of biomarker")
+
+hist(male_data[['CXCL9']],main = "Male data CXCL9",xlab = "Levels of biomarker")
+
+hist(female_data[['CXCL9']],main = "Female data CXCL9",xlab = "Levels of biomarker")
+
+hist(male_data[['CXCL1']],main = "Male data CXCL1",xlab = "Levels of biomarker")
+
+hist(female_data[['CXCL1']],main = "Female data CXCL1",xlab = "Levels of biomarker")
+
+hist(male_data[['IL-18']],main = "Male data IL-18",xlab = "Levels of biomarker")
+
+hist(female_data[['IL-18']],main = "Female data IL-18",xlab = "Levels of biomarker")
+
+hist(male_data[['CSF-1']],main = "Male data CSF-1",xlab = "Levels of biomarker")
+
+hist(female_data[['CSF-1']],main = "Female data CSF-1",xlab = "Levels of biomarker")
+
+
+dev.off()
 
 
 
@@ -114,5 +156,3 @@ female_averages <- c(IL_8$estimate[2],VegaF_A$estimate[2],OPG$estimate[2],TGF$es
 print(male_averages)
 print(female_averages)
 
-# hist(male_data_inclusion[['VEGF-A']])
-# hist(female_data_inclusion[['VEGF-A']])
