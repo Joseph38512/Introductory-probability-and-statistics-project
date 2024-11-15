89fdfd484b55d1792569182c5aef63b53681eae9
Joseph38512 <s1910643@ed.ac.uk>
2024-11-15 14:14:32 +0000

    Final version

--- code_for_data.r
+++ code_for_data.r
@@ -1,4 +1,4 @@
-
+#Import the required libraries.
 library(readxl)
 library(data.table)
 
@@ -52,21 +52,25 @@ model <- lm(`Vas-12months` ~ `Sex (1=male, 2=female)`+ Age +`Smoker (1=yes, 2=no
 #Use the model to predict the next values.
 predictions <- predict(model, newdata = twenty_percent_covariates)
 
-#Print the differences in value.
-print(c(abs(predictions - twenty_percent_covariates["Vas-12months"])))
+#Find the difference and then print them.
+difference_values <- abs(predictions - twenty_percent_covariates[["Vas-12months"]])
+print(difference_values)
+
+
 
 #Print the average difference.
-print(mean(c(abs(predictions - twenty_percent_covariates["Vas-12months"]))))
+print(mean(difference_values))
+
 
 #Print first 7 to be put into a table to show prediction.
 print(twenty_percent_covariates[1:7,c(1, 6)])
 
 
-
+#Match the data where males and females ID matches the patient ID.
 male_indices <- data1[,PatientID %in% males_ID]
 female_indices <- data1[,PatientID %in% females_ID]
 
-
+#Using the indices of male and female data create two new dataframes of male and female data.
 male_data <- data1[male_indices]
 female_data <- data1[female_indices]
 
@@ -88,46 +92,46 @@ IL_18 <- t.test(male_data[['IL-18']],female_data[['IL-18']])
 CSF_1 <- t.test(male_data[['CSF-1']],female_data[['CSF-1']])
 
 
-#Output the distrubutions.
+#Output the distrubutions of each of the biomarkers for males and females as histograms.
 dev.new(width=5, height=10)
 png(filename = "plot_of_distrubutions.png",width = 600, height = 800)
 par(mfrow = c(5, 4))
 
-hist(male_data[['IL-8']],main = "Male data IL-8",xlab = "Levels of biomarker")
+hist(male_data[['IL-8']],main = "Male data IL-8",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['IL-8']],main = "Female data IL-8",xlab = "Levels of biomarker")
+hist(female_data[['IL-8']],main = "Female data IL-8",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['VEGF-A']],main = "Male data VEGF-A",xlab = "Levels of biomarker")
+hist(male_data[['VEGF-A']],main = "Male data VEGF-A",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['VEGF-A']],main = "Female data VEGF-A",xlab = "Levels of biomarker")
+hist(female_data[['VEGF-A']],main = "Female data VEGF-A",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['OPG']],main = "Male data OPG",xlab = "Levels of biomarker")
+hist(male_data[['OPG']],main = "Male data OPG",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['OPG']],main = "Female data OPG",xlab = "Levels of biomarker")
+hist(female_data[['OPG']],main = "Female data OPG",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['TGF-beta-1']],main = "Male data TGF-beta-1",xlab = "Levels of biomarker")
+hist(male_data[['TGF-beta-1']],main = "Male data TGF-beta-1",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['TGF-beta-1']],main = "Female data TGF-beta-1",xlab = "Levels of biomarker")
+hist(female_data[['TGF-beta-1']],main = "Female data TGF-beta-1",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['IL-6']],main = "Male data IL-6",xlab = "Levels of biomarker")
+hist(male_data[['IL-6']],main = "Male data IL-6",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['IL-6']],main = "Female data IL-6",xlab = "Levels of biomarker")
+hist(female_data[['IL-6']],main = "Female data IL-6",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['CXCL9']],main = "Male data CXCL9",xlab = "Levels of biomarker")
+hist(male_data[['CXCL9']],main = "Male data CXCL9",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['CXCL9']],main = "Female data CXCL9",xlab = "Levels of biomarker")
+hist(female_data[['CXCL9']],main = "Female data CXCL9",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['CXCL1']],main = "Male data CXCL1",xlab = "Levels of biomarker")
+hist(male_data[['CXCL1']],main = "Male data CXCL1",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['CXCL1']],main = "Female data CXCL1",xlab = "Levels of biomarker")
+hist(female_data[['CXCL1']],main = "Female data CXCL1",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['IL-18']],main = "Male data IL-18",xlab = "Levels of biomarker")
+hist(male_data[['IL-18']],main = "Male data IL-18",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['IL-18']],main = "Female data IL-18",xlab = "Levels of biomarker")
+hist(female_data[['IL-18']],main = "Female data IL-18",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(male_data[['CSF-1']],main = "Male data CSF-1",xlab = "Levels of biomarker")
+hist(male_data[['CSF-1']],main = "Male data CSF-1",xlab = "Levels of biomarker",xlim = c(0,14))
 
-hist(female_data[['CSF-1']],main = "Female data CSF-1",xlab = "Levels of biomarker")
+hist(female_data[['CSF-1']],main = "Female data CSF-1",xlab = "Levels of biomarker",xlim = c(0,14))
 
 
 dev.off()
@@ -147,12 +151,15 @@ print(p_values)
 
 
 
-
+#Calculate the t test estimates for the averages of males for each biomarker.
 male_averages <- c(IL_8$estimate[1],VegaF_A$estimate[1],OPG$estimate[1],TGF$estimate[1],IL_6$estimate[1]
 ,CXCL9$estimate[1],CXCL1$estimate[1],IL_18$estimate[1],CSF_1$estimate[1])
 
+#Calculate the t test estimates for the averages of males for each biomarker.
 female_averages <- c(IL_8$estimate[2],VegaF_A$estimate[2],OPG$estimate[2],TGF$estimate[2],IL_6$estimate[2]
 ,CXCL9$estimate[2],CXCL1$estimate[2],IL_18$estimate[2],CSF_1$estimate[2])
+
+#Print the values calculated above to put into a table.
 print(male_averages)
 print(female_averages)
 
